b188a741b4fb1abd1b44d980145f4172b5d5cbe3
okazaki_takumi <takumicr1526@gmail.com>
2024-08-22 23:52:25 +0900

    ヘッダー　ログイン画面作成

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -4,24 +4,24 @@ import { fetcher } from '@/utils'
 import camelcaseKeys from 'camelcase-keys'
 import { PostType } from '@/types/Post'
 import Image from 'next/image'
+import Loading from '@/components/Loading'
 
 const Index: NextPage = () => {
   const url = process.env.NEXT_PUBLIC_API_BASE_URL + '/posts'
   const { data, error } = useSWR(url, fetcher)
 
   if (error) return <div className='text-orange-300'>An error has occurred.</div>
-  if (!data) return <>Loading...</>
+  if (!data) return <Loading/>
 
   const posts = camelcaseKeys(data)
 
   return (
     <>
-      <div className='text-orange-300'>Rails疎通確認</div>
+      <div className='text-w-bold'>投稿</div>
       <div>
         {posts.map((post: PostType, i: number) => (
          <div key={i}>
           <p>{post.creatureName}</p>
-          <p>{post.imageUrl}</p>
           <Image
             src={post.imageUrl}
             width={200}
