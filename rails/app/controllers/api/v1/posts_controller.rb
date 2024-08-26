a7b60c69ae9ef6f53e41e441e24416095a964223
okazaki_takumi <takumicr1526@gmail.com>
2024-08-26 22:02:54 +0900

    ページネーションの作成

--- rails/app/controllers/api/v1/posts_controller.rb
+++ rails/app/controllers/api/v1/posts_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::PostsController < ApplicationController
+    include Pagination
+
     def show
         post = Post.find(params[:id])
         render json: post
     end
 
     def index
-        posts = Post.all
-        render json: posts
+        posts = Post.all.page(params[:page] || 1).per(10)
+        render json: posts, meta: pagination(posts), adapter: :json
     end
     
 end
